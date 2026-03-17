63b5b1e5c68d0c313b964da4f24e4673c4c2a26c
Sipros <sipros333@gmail.com>
2026-03-17 16:11:36 +0100

    Change puppeteer to puppeteer-core dependency

--- supabase_scraper_fixed_urls.js
+++ supabase_scraper_fixed_urls.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 const fs = require('fs');
 const https = require('https');
 const { createClient } = require('@supabase/supabase-js');
